0d2f241ba25780ec45ec3d864e7bdd2739c9e77a
hehcker <nedokakaa@gmail.com>
2025-12-22 17:45:17 +0200

    add more brands to getMarketingName

--- app/src/main/java/com/hehcker/deviceinfo/data/device/ModelProvider.kt
+++ app/src/main/java/com/hehcker/deviceinfo/data/device/ModelProvider.kt
@@ -8,20 +8,69 @@ import android.provider.Settings
 object ModelProvider {
 
     fun getMarketingName(context: Context): String? {
-        val manufacturer = Build.MANUFACTURER.lowercase()
+        val brand = Build.BRAND.lowercase()
 
-        return when (manufacturer) {
+        val brandMarketingName = when (brand) {
             "xiaomi", "redmi", "poco" -> getXiaomiName()
             "samsung" -> getSamsungName(context)
+            "sony" -> getSonyName()
+            "huawei", "honor" -> getHuaweiName()
+            "oneplus", "realme", "oppo" -> getOplusName()
+            "vivo" -> getVivoName()
 
             // to be filled
             else -> null
         }
+
+        val marketingName = brandMarketingName ?: getGenericFallback()
+
+        return marketingName?.takeIf { !marketingName.equals(Build.MODEL, ignoreCase = true) }
+    }
+
+    private fun getGenericFallback(): String? {
+
+        val genericProperties = listOf(
+            "ro.product.marketname",
+            "ro.product.display",
+            "ro.display.series",
+            "ro.config.marketing_name",
+            "ro.config.devicename"
+        )
+
+        for (property in genericProperties) {
+            val name = SystemProperties.get(property)
+            if (name != null) return name
+        }
+        return null
     }
 
     private fun getXiaomiName(): String? {
         return SystemProperties.get("ro.product.marketname")
             .takeIf { it.isNotBlank() }
+    }
+    private fun getSonyName(): String? {
+        return SystemProperties.get("ro.semc.product.name")
+            .takeIf { it.isNotBlank() }
+    }
+
+    private fun getHuaweiName(): String? {
+        return SystemProperties.get("ro.config.marketing_name")
+            .takeIf { it.isNotBlank() }
+    }
+
+    private fun getOplusName(): String? {
+        return SystemProperties.get("ro.vendor.oplus.market.name")
+            .takeIf { it.isNotBlank() }
+    }
+
+    private fun getVivoName(): String? {
+        return SystemProperties.get("ro.vivo.camera.watermark")
+            .takeIf { it.isNotBlank() } ?:
+            SystemProperties.get("ro.vivo.market.name")
+            .takeIf { it.isNotBlank() } ?:
+            SystemProperties.get("ro.vivo.product.release.name")
+            .takeIf { it.isNotBlank() }
+
     }
 
     private fun getSamsungName(context: Context): String? {
